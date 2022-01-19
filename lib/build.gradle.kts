0e71a37ccc8f850925b587fa106f8c7d60b21f1e
Pierre-Yves Ricau <py@squareup.com>
2022-01-19 10:27:36 -0800

    Generate SimpleperfReport

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation("com.google.guava:guava:30.1.1-jre")
 
+    implementation("com.google.protobuf:protobuf-java:3.19.3")
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
